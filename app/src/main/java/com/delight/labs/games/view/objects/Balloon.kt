0bc3530c259747e85c88e17fb7d423faacdc5e44
Erick Mwangi <erick@lisheliving.com>
2021-03-31 00:31:26 +0300

    adds floating mode layout

---
+++ app/src/main/java/com/delight/labs/games/view/objects/Balloon.kt
@@ -0,0 +1,76 @@
+package com.delight.labs.games.view.objects
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.animation.ValueAnimator.AnimatorUpdateListener
+import android.content.Context
+import android.view.MotionEvent
+import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import androidx.appcompat.widget.AppCompatImageView
+import com.delight.labs.games.R
+import com.delight.labs.games.helper.utils.pixelsToDp
+
+class Balloon : AppCompatImageView, Animator.AnimatorListener,
+    AnimatorUpdateListener {
+    private var mAnimator: ValueAnimator? = null
+    private var mListener: BalloonListener? = null
+    private var mPopped = false
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, color: Int, rawHeight: Int) : super(context) {
+        mListener = context as BalloonListener?
+        setImageResource(R.drawable.balloon)
+        this.setColorFilter(color)
+        val rawWidth = rawHeight / 2
+        val dpHeight: Int = pixelsToDp(rawHeight, context)
+        val dpWidth: Int = pixelsToDp(rawWidth, context)
+        val params = ViewGroup.LayoutParams(dpWidth, dpHeight)
+        layoutParams = params
+    }
+
+    fun releaseBalloon(screenHeight: Int, duration: Int) {
+        mAnimator = ValueAnimator()
+        mAnimator!!.duration = duration.toLong()
+        mAnimator!!.setFloatValues(screenHeight.toFloat(), 0f)
+        mAnimator!!.interpolator = LinearInterpolator()
+        mAnimator!!.setTarget(this)
+        mAnimator!!.addListener(this)
+        mAnimator!!.addUpdateListener(this)
+        mAnimator!!.start()
+    }
+
+    override fun onAnimationStart(animation: Animator) {}
+    override fun onAnimationEnd(animation: Animator) {
+        if (!mPopped) {
+            mListener!!.popBalloon(this, false)
+        }
+    }
+
+    override fun onAnimationCancel(animation: Animator) {}
+    override fun onAnimationRepeat(animation: Animator) {}
+    override fun onAnimationUpdate(animation: ValueAnimator) {
+        y = (animation.animatedValue as Float)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!mPopped && event.action == MotionEvent.ACTION_DOWN) {
+            mListener!!.popBalloon(this, true)
+            mPopped = true
+            mAnimator!!.cancel()
+        }
+        return super.onTouchEvent(event)
+    }
+
+    fun setPopped(b: Boolean) {
+        mPopped = true
+        if (b) {
+            mAnimator!!.cancel()
+        }
+    }
+
+    interface BalloonListener {
+        fun popBalloon(balloon: Balloon?, userTouch: Boolean)
+    }
+}
