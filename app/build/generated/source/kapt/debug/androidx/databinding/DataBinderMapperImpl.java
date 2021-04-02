0b617800031e50f47d0e8f54922af2154c31114d
Erick Mwangi <erick@lisheliving.com>
2021-04-02 21:23:48 +0300

    removes build folders

--- app/build/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,8 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.delight.labs.games.DataBinderMapperImpl());
-    addMapper(new V1CompatDataBinderMapperImpl());
-  }
-}
